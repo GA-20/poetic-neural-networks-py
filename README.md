05711861f6344182154d5bda9fd6f807d511b4fd
Julio DEV <julio.f@sdsharp.com>
2024-06-19 17:46:18 -0400

    feat: example

--- README.md
+++ README.md
@@ -8,6 +8,40 @@ The resource I used to train the neural network is the [NeuralNine](https://www.
 
 The code is overcommented to help me understand what's going on and to help others who are learning about neural networks, so if you're an expert, I apologize for the verbosity.
 
+## LSTM
+
+![LSTM](https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/LSTM_Cell.svg/1920px-LSTM_Cell.svg.png)
+
+LSTMs are a special kind of neural network architecture called a recurrent neural network (RNN). RNNs are designed to handle sequential data, where the order of elements matters.
+
+LSTM can potentially retain that context through its cell state and use it to make a more accurate prediction.
+
+Since recurrent neural networks and LSTMs in particular have a short term memory, we can train it to “guess” the next letter based on the letters that came before. That leads to our network producing these texts. For this of course we will need some substantial training data.
+
+## Text to numerical
+
+We cannot just train a neural network on letters or sentences. We need to convert all of these values into numerical data.
+
+So we have to come up with a system that allows us to convert the text into numbers, then predict specific numbers based on that data and then again convert the resulting numbers back into text.
+
+### Text Preprocessing
+
+Text processing can include many transformations of text, such as:
+
+- Remove unnecessary characters like punctuation marks, symbols, or special characters (depending on needs).
+- Convert text to lowercase for consistency.
+- Stemming or lemmatization to reduce words to their root form.
+
+In this exercise, the lowercase technique is the only one used.
+
+### Text Tokenization
+
+Break down the text into smaller units. This could be words, characters, or n-grams (sequences of n consecutive words). For this example the text size is reduced to avoid make the problem a bit complex at this point.
+
+### Vocabulary Building
+
+Create a dictionary (vocabulary) that maps each unique token (word, character, or n-gram) to a unique integer identifier. This allows the network to recognize different tokens.
+
 ## How to install
 
 ```bash
--- main.py
+++ main.py
@@ -1,14 +1,105 @@
-import random  # Generating random numbers
-import numpy as np  # It offers efficient arrays and mathematical operations
-import tensorflow as tf  #  Framework for machine learning and deep learning
+import random
+import numpy as np
 
-# Downloads a text file containing Shakespeare's works and prepares it for use.
+#
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import Activation, Dense, LSTM
+
+# STEP 1
 filepath = tf.keras.utils.get_file(
     "shakespeare.txt",
     "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt",
 )
 
-# Transforms the downloaded data into a usable format
-# filepath is the path to the text file
-# the "rb" parameter indicates that the file is read in binary mode
-text = open(filepath, "rb").read().decode(encoding="utf-8")
+# STEP 2
+text = open(filepath, "rb").read().decode(encoding="utf-8").lower()
+text = text[300000:800000]
+
+
+# STEP 3
+characters = sorted(set(text))
+char_to_index = dict((c, i) for i, c in enumerate(characters))
+index_to_char = dict((i, c) for i, c in enumerate(characters))
+
+
+# STEP 5
+SEQ_LENGTH = 40
+STEP_SIZE = 3
+sentences = []
+next_char = []
+
+# STEP 6
+for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
+    sentences.append(text[i : i + SEQ_LENGTH])
+    next_char.append(text[i + SEQ_LENGTH])
+
+
+# STEP 7
+x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool)
+y = np.zeros((len(sentences), len(characters)), dtype=np.bool)
+
+# STEP 8
+for i, satz in enumerate(sentences):
+    for t, char in enumerate(satz):
+        x[i, t, char_to_index[char]] = 1
+    y[i, char_to_index[next_char[i]]] = 1
+
+
+# STEP 9
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(SEQ_LENGTH, len(characters))))
+model.add(Dense(len(characters)))
+model.add(Activation("softmax"))
+
+
+# STEP 10
+
+model.compile(loss="categorical_crossentropy", optimizer=RMSprop(lr=0.01))
+model.fit(x, y, batch_size=256, epochs=4)
+
+
+# HELPERS
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype("float64")
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+# INTERFACE
+def generate_text(length, temperature):
+    start_index = random.randint(0, len(text) - SEQ_LENGTH - 1)
+    generated = ""
+    sentence = text[start_index : start_index + SEQ_LENGTH]
+    generated += sentence
+    for i in range(length):
+        x_predictions = np.zeros((1, SEQ_LENGTH, len(characters)))
+        for t, char in enumerate(sentence):
+            x_predictions[0, t, char_to_index[char]] = 1
+
+        predictions = model.predict(x_predictions, verbose=0)[0]
+        next_index = sample(predictions, temperature)
+        next_character = index_to_char[next_index]
+
+        generated += next_character
+        sentence = sentence[1:] + next_character
+    return generated
+
+def main():
+    print(generate_text(300, 0.2))
+    print(generate_text(300, 0.4))
+    print(generate_text(300, 0.5))
+    print(generate_text(300, 0.6))
+    print(generate_text(300, 0.7))
+    print(generate_text(300, 0.8))
+
+
+
+if __name__ == '__main__':
+    main()
+
